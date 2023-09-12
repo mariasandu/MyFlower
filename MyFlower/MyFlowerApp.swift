2a7ee1148dbbb8719b362114d3e4b58cd74415dd
Maria Sandu <Maria.Sandu@emids.com>
2023-09-12 13:33:00 +0300

    Initial Commit

---
+++ MyFlower/MyFlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFlowerApp.swift
+//  MyFlower
+//
+//  Created by Maria Sandu on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
